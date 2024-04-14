32a1c17b8c850c1a9a22f0b738c2ebd6c2264ebd
antonioneves <antoniocalheirosneves@hotmail.com>
2024-04-14 14:59:33 +0100

    Changed to hide section old layout to

--- content.js
+++ content.js
@@ -1,10 +1,17 @@
 // This function hides the specified section
 function hideBottomGrid() {
   const bottomGrid = document.getElementById('bottom-grid');
+  const related = document.getElementById('related')
+
   if (bottomGrid) {
     bottomGrid.style.display = 'none';
   }
+
+  if (related) {
+    related.style.display = 'none';
+  }
 }
+
 
 // Listen for changes in the DOM
 const observer = new MutationObserver(hideBottomGrid);
